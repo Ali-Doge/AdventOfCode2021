00f441f54a82b139f890f07834605de55404606a
Ali-Doge <as2822@rit.edu>
2022-02-08 23:08:51 -0500

    Completed Day 2!

--- AoC/day2/PositionCalc.java
+++ AoC/day2/PositionCalc.java
@@ -1,3 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class PositionCalc{
-    
+    public static void Calc()throws FileNotFoundException{
+        int depth = 0;
+        int x = 0;
+        int aim = 0;
+        Scanner scan = new Scanner(new File("input.txt"));
+        while (scan.hasNextLine()){
+            String line = scan.nextLine();
+            int num = Integer.parseInt(String.valueOf(line.charAt(line.length()-1)));
+            if (line.startsWith("f")){x += num; depth += aim*num;}
+            if (line.startsWith("u")){ aim -= num;}
+            if (line.startsWith("d")){ aim += num;}
+        }
+        System.out.println(x*depth);
+    }
+    public static void main(String[] args) throws FileNotFoundException{
+        Calc();
+    } 
 }
