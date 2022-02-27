b451678e5e6a39c49ce96b5c850690ea27575727
Ali-Doge <as2822@rit.edu>
2022-02-27 01:30:14 -0500

    Added Last changes

--- AoC/day3/PowerCalc.java
+++ AoC/day3/PowerCalc.java
@@ -1,25 +1,30 @@
 package day3;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class PowerCalc{
     public static void Calc()throws FileNotFoundException{
-        int[] gamma = new int[12];
+        int lenght = 12;
+        int[] gamma = new int[lenght];
         int gam = 0;
-        int[] epsi = new int[12];
+        int[] epsi = new int[lenght];
         int eps = 0;
         int i = 0;
+        ArrayList<String> strings = new ArrayList<>();
+
         Scanner scan = new Scanner(new File("input.txt"));
         while (scan.hasNextLine()){
             i++;
             String line = scan.nextLine();
-            for (int j = 0; j<12; j++){
+            strings.add(line);
+            for (int j = lenght-1; j>=0; j--){
                 gamma[j] += Integer.parseInt(String.valueOf(line.charAt(j)));
             }
         }
-        for (int j = 0; j<12; j++){
+        for (int j = 0; j<lenght; j++){
             if (gamma[j] > i/2){
                 gamma[j] = 1;
                 epsi[j] = 0;
@@ -28,10 +33,60 @@ public class PowerCalc{
                 gamma[j] = 0;
                 epsi[j] = 1;
             }
-            gam += gamma[j] * 2^j;
+            gam += gamma[j] * Math.pow(2,lenght-1-j);
+            eps += epsi[j] * Math.pow(2,lenght-1-j);
         }
-        System.out.println(gam * (2^12 - 1 -gam));
-    }    
+        System.out.println(gam * eps);
+        int OGen = mostCommonFinder(strings, 0);
+        int CO2 = leastCommonFinder(strings, 0);
+        System.out.println(OGen * CO2);
+    }  
+    
+    public static int mostCommonFinder(ArrayList<String> search, int bit){
+        ArrayList<String> ones = new ArrayList<>();
+        ArrayList<String> zeros = new ArrayList<>();
+        if (search.size() == 1){
+            return Integer.parseInt(search.get(0),2);
+        }
+        for (String element : search){
+            if (element.charAt(bit) == '1'){
+                ones.add(element);
+            }else{
+                zeros.add(element);
+            }
+        }
+        if (ones.size() == zeros.size()){
+            return mostCommonFinder(ones, bit+1);
+        }
+        if (ones.size() > (double) search.size()/2){
+            return mostCommonFinder(ones, bit+1);
+        }else{
+            return mostCommonFinder(zeros, bit+1);
+        }
+    }
+
+    public static int leastCommonFinder(ArrayList<String> search, int bit){
+        ArrayList<String> ones = new ArrayList<>();
+        ArrayList<String> zeros = new ArrayList<>();
+        if (search.size() == 1){
+            return Integer.parseInt(search.get(0),2);
+        }
+        for (String element : search){
+            if (element.charAt(bit) == '1'){
+                ones.add(element);
+            }else{
+                zeros.add(element);
+            }
+        }
+        if (ones.size() == zeros.size()){
+            return leastCommonFinder(zeros, bit+1);
+        }
+        if (ones.size() < (double) search.size()/2){
+            return leastCommonFinder(ones, bit+1);
+        }else{
+            return leastCommonFinder(zeros, bit+1);
+        }
+    }
 
     public static void main(String[] args) throws FileNotFoundException{
         Calc();
